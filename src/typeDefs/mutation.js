c4fb6ac91c1d2dd6abefa41e36a46bc9542cb0ce
Andrew Davisson <akdavisson4@gmail.com>
2020-03-26 13:57:27 -0600

    changed newContact to createContact

--- src/typeDefs/mutation.js
+++ src/typeDefs/mutation.js
@@ -4,7 +4,7 @@ const mutation = gql`
   type Mutation {
     signup(email: String!, password: String!, name: String!, bio: String, age: Int, gender: String): AuthPayload
     login(email: String!, password: String!): AuthPayload
-    newContact(name: String!, email: String, phoneNumber: String, jobTitle: String): Contact
+    createContact(name: String!, email: String, phoneNumber: String, jobTitle: String): Contact
   }
 `
 
