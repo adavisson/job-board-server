1575a5563a953efeba36c929201e90909da4cc19
Andrew Davisson <akdavisson4@gmail.com>
2020-03-26 09:00:48 -0600

    added dotenv

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const { GraphQLServer } = require('graphql-yoga');
 const port = 4000;
 const { typeDefs } = require('./typeDefs');
 const { resolvers } = require('./resolvers');
+const { dotenv } = require('dotenv');
 
 const server = new GraphQLServer({
   typeDefs: typeDefs,
