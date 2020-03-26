097588a67d52b3b4a4db01384d6de4f4ba5eb5c2
Andrew Davisson <akdavisson4@gmail.com>
2020-03-26 09:58:24 -0600

    added context to graphql server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { GraphQLServer } = require('graphql-yoga');
 const port = 4000;
+const { prisma } = require('./generated/prisma-client');
 const { typeDefs } = require('./typeDefs');
 const { resolvers } = require('./resolvers');
 const { dotenv } = require('dotenv');
@@ -7,6 +8,11 @@ const { dotenv } = require('dotenv');
 const server = new GraphQLServer({
   typeDefs: typeDefs,
   resolvers: resolvers,
-  //Add context
+  context: request => {
+    return {
+      ...request,
+      prisma
+    }
+  }
 });
 server.start(({port: port}) => console.log(`Server started on http://localhost:${port}`));
