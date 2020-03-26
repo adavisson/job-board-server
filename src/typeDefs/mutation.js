a3c76dc118e41d21134d5763b6fb88c8a1f0b3f9
Andrew Davisson <akdavisson4@gmail.com>
2020-03-26 15:27:08 -0600

    added createCompany mutation

--- src/typeDefs/mutation.js
+++ src/typeDefs/mutation.js
@@ -5,6 +5,7 @@ const mutation = gql`
     signup(email: String!, password: String!, name: String!, bio: String, age: Int, gender: String): AuthPayload
     login(email: String!, password: String!): AuthPayload
     createContact(name: String!, email: String, phoneNumber: String, jobTitle: String): Contact
+    createCompany(name: String!, address: String, phoneNumber: String, website: String): Company
   }
 `
 
