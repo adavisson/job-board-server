1f8f87f7dba301dad1559b50967dc761aed93450
Andrew Davisson <akdavisson4@gmail.com>
2020-03-26 22:42:58 -0600

    importe job posting resolvers

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -2,8 +2,9 @@ const { helloResolver } = require('./helloResolver');
 const { userResolvers } = require('./userResolvers');
 const { contactResolvers } = require('./contactResolvers');
 const { companyResolvers } = require('./companyResolvers');
+const { jobPostingResolvers } = require('./jobPostingResolvers');
 
-const resolvers = [contactResolvers, companyResolvers, helloResolver, userResolvers];
+const resolvers = [contactResolvers, companyResolvers, helloResolver, userResolvers, jobPostingResolvers];
 
 module.exports = {
   resolvers,
