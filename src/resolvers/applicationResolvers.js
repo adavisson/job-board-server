f2887b4cfab565a71e8ea9eedd6458f7b27f0d1d
Andrew Davisson <akdavisson4@gmail.com>
2020-03-27 10:01:39 -0600

    added jobPosting resolver

--- src/resolvers/applicationResolvers.js
+++ src/resolvers/applicationResolvers.js
@@ -1,14 +1,28 @@
-const { getUserId } = require('../utils');
+const { getUserId } = require("../utils");
 
 const applicationResolvers = {
+  Application: {
+    notes: (parent, args, context, info) => {
+      const userId = getUserId(context);
+      const where = {
+        user: {id: userId}
+      }
+      const notes = context.prisma.application({id: parent.id}).notes({where})
+      return notes
+    },
+    jobPosting: (parent, args, context, info) => {
+      const jobPosting = context.prisma.application({id: parent.id}).jobPosting();
+      return jobPosting
+    }
+  },
   Query: {
     applications: (parent, args, context, info) => {
       const userId = getUserId(context);
       const where = {
         user: { id: userId }
-      }
-      const applications = context.prisma.applications({where})
-      return applications
+      };
+      const applications = context.prisma.applications({ where });
+      return applications;
     }
   },
   Mutation: {
@@ -17,12 +31,12 @@ const applicationResolvers = {
       const application = await context.prisma.createApplication({
         user: { connect: { id: userId } },
         jobPosting: { connect: { id: args.jobPostingId } }
-      })
-      return application
+      });
+      return application;
     }
   }
-}
+};
 
 module.exports = {
-  applicationResolvers,
-}
+  applicationResolvers
+};
