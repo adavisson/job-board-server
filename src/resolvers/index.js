5ca315e279618a16e67f3367100b8ac7a03edfb7
Andrew Davisson <akdavisson4@gmail.com>
2020-03-26 23:12:32 -0600

    imported applicationResolvers

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -2,8 +2,9 @@ const { userResolvers } = require('./userResolvers');
 const { contactResolvers } = require('./contactResolvers');
 const { companyResolvers } = require('./companyResolvers');
 const { jobPostingResolvers } = require('./jobPostingResolvers');
+const { applicationResolvers } = require('./applicationResolvers');
 
-const resolvers = [contactResolvers, companyResolvers, userResolvers, jobPostingResolvers];
+const resolvers = [contactResolvers, companyResolvers, userResolvers, jobPostingResolvers, applicationResolvers];
 
 module.exports = {
   resolvers,
