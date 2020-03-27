2790d0a655126c06d5afcda88576ae3543bb7380
Andrew Davisson <akdavisson4@gmail.com>
2020-03-26 23:51:39 -0600

    imported noteResolvers

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -3,6 +3,7 @@ const { contactResolvers } = require('./contactResolvers');
 const { companyResolvers } = require('./companyResolvers');
 const { jobPostingResolvers } = require('./jobPostingResolvers');
 const { applicationResolvers } = require('./applicationResolvers');
+const { noteResolvers } = require('./noteResolvers');
 
 const resolvers = [contactResolvers, companyResolvers, userResolvers, jobPostingResolvers, applicationResolvers];
 
