6dd93b6128e73af1c5384725556a06b122b08a7e
Andrew Davisson <akdavisson4@gmail.com>
2020-03-26 23:20:41 -0600

    added applications query and createApplication

--- src/resolvers/applicationResolvers.js
+++ src/resolvers/applicationResolvers.js
@@ -2,10 +2,20 @@ const { getUserId } = require('../utils');
 
 const applicationResolvers = {
   Query: {
-
+    applications: (parent, args, context, info) => {
+      const userId = getUserId(context);
+      const applications = context.prisma.applications({ user: { id: userId } })
+      return applications
+    }
   },
   Mutation: {
-
+    createApplication: async (parent, args, context, info) => {
+      const userId = getUserId(context);
+      const application = await context.prisma.createApplication({
+        user: { connect: { id: userId } },
+        jobPosting: { connect: { id: args.jobPostingId } }
+      })
+    }
   }
 }
 
