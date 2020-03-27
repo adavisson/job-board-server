5f80d080a2e27727e6aabb15e02548fde4341d8d
Andrew Davisson <akdavisson4@gmail.com>
2020-03-26 22:44:52 -0600

    removed hello query

--- src/typeDefs/query.js
+++ src/typeDefs/query.js
@@ -2,7 +2,6 @@ const gql = require('graphql-tag');
 
 const query = gql`
   type Query {
-    hello: String!
     name: String!
     contacts: [Contact!]!
     employees(companyId: ID!): [Contact!]!
