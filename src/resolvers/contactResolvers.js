65c973e942f953409596dd943a1bf87a703aaf17
Andrew Davisson <akdavisson4@gmail.com>
2020-03-27 10:16:37 -0600

    added notes resolver

--- src/resolvers/contactResolvers.js
+++ src/resolvers/contactResolvers.js
@@ -1,6 +1,20 @@
 const { getUserId } = require('../utils');
 
 const contactResolvers = {
+  Contact: {
+    company: (parent, args, context, info) => {
+      const company = context.prisma.contact({id: parent.id}).company()
+      return company
+    },
+    notes: (parent, args, context, info) => {
+      const userId = getUserId(context);
+      const where = {
+        user: {id: userId}
+      }
+      const notes = context.prisma.contact({id: parent.id}).notes({where})
+      return notes
+    }
+  },
   Mutation: {
     createContact: async (parent, args, context, info) => {
       const userId = getUserId(context)
