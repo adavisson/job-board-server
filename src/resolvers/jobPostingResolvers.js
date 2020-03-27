805e51ce3b569c13426bcb972b3f91bfb5336cde
Andrew Davisson <akdavisson4@gmail.com>
2020-03-26 22:54:37 -0600

    added createJobPosting mutation

--- src/resolvers/jobPostingResolvers.js
+++ src/resolvers/jobPostingResolvers.js
@@ -2,10 +2,20 @@ const { getUserId } = require('../utils')
 
 const jobPostingResolvers = {
   Query: {
-
+    jobPostings: (parent, args, context, info) => {
+      return context.prisma.jobPostings()
+    }
   },
   Mutation: {
-
+    createJobPosting: async (parent, args, context, info) => {
+      const userId = getUserId(context)
+      const jobPosting = await context.prisma.createJobPosting({
+        title: args.title,
+        link: args.link,
+        company: { connect: { id: args.companyId }}
+      })
+      return jobPosting
+    }
   }
 }
 
