3540a771eed424e0c1647f066efe42eadf86df08
Andrew Davisson <akdavisson4@gmail.com>
2020-03-26 22:44:19 -0600

    removed helloResolver

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,10 +1,9 @@
-const { helloResolver } = require('./helloResolver');
 const { userResolvers } = require('./userResolvers');
 const { contactResolvers } = require('./contactResolvers');
 const { companyResolvers } = require('./companyResolvers');
 const { jobPostingResolvers } = require('./jobPostingResolvers');
 
-const resolvers = [contactResolvers, companyResolvers, helloResolver, userResolvers, jobPostingResolvers];
+const resolvers = [contactResolvers, companyResolvers, userResolvers, jobPostingResolvers];
 
 module.exports = {
   resolvers,
