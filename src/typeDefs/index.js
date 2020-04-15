21822ef48b19680a85d2cb63769e704e7d9f1c75
Andrew Davisson <akdavisson4@gmail.com>
2020-04-14 23:16:22 -0600

    format

--- src/typeDefs/index.js
+++ src/typeDefs/index.js
@@ -1,8 +1,26 @@
-const { query } = require('./query');
-const { mutation } = require('./mutation');
-const { userType, authPayloadType, contactType, companyType, jobPostingType, applicationType, noteType} = require('./types');
+const { query } = require('./query')
+const { mutation } = require('./mutation')
+const {
+  userType,
+  authPayloadType,
+  contactType,
+  companyType,
+  jobPostingType,
+  applicationType,
+  noteType,
+} = require('./types')
 
-const typeDefs = [query, mutation, userType, authPayloadType, contactType, companyType, jobPostingType, applicationType, noteType];
+const typeDefs = [
+  query,
+  mutation,
+  userType,
+  authPayloadType,
+  contactType,
+  companyType,
+  jobPostingType,
+  applicationType,
+  noteType,
+]
 
 module.exports = {
   typeDefs,
