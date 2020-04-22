cbc73e35c86a2f278ceeb1cc06de03e378763b8a
Andrew Davisson <akdavisson4@gmail.com>
2020-04-22 13:20:04 -0600

    prettier

--- src/resolvers/contactResolvers.js
+++ src/resolvers/contactResolvers.js
@@ -1,33 +1,33 @@
-const { getUserId } = require('../utils');
+const { getUserId } = require('../utils')
 
 const contactResolvers = {
   Contact: {
     company: (parent, args, context, info) => {
-      const company = context.prisma.contact({id: parent.id}).company()
+      const company = context.prisma.contact({ id: parent.id }).company()
       return company
     },
     notes: (parent, args, context, info) => {
-      const userId = getUserId(context);
+      const userId = getUserId(context)
       const where = {
-        user: {id: userId}
+        user: { id: userId },
       }
-      const notes = context.prisma.contact({id: parent.id}).notes({where})
+      const notes = context.prisma.contact({ id: parent.id }).notes({ where })
       return notes
     },
     user: (parent, args, context, info) => {
-      const userId = getUserId(context);
-      return context.prisma.contact({id: parent.id}).user()
-    }
+      const userId = getUserId(context)
+      return context.prisma.contact({ id: parent.id }).user()
+    },
   },
   Query: {
     contact: async (parent, args, context, info) => {
-      const userId = getUserId(context);
-      const contacts = await context.prisma.user({id: userId}).contacts()
+      const userId = getUserId(context)
+      const contacts = await context.prisma.user({ id: userId }).contacts()
       const contact = contacts.find((contact) => contact.id === args.id)
       return contact
     },
     contacts: (parent, args, context, info) => {
-      const userId = getUserId(context);
+      const userId = getUserId(context)
       return context.prisma.user({ id: userId }).contacts()
     },
   },
@@ -41,32 +41,32 @@ const contactResolvers = {
         jobTitle: args.jobTitle,
       }
       if (args.companyId) {
-          contactObject.company = {connect: { id: args.companyId }}
+        contactObject.company = { connect: { id: args.companyId } }
       }
       const contact = await context.prisma.createContact({
         ...contactObject,
-        user: { connect: { id: userId }}
+        user: { connect: { id: userId } },
       })
       return contact
     },
     deleteContact: async (parent, args, context, info) => {
       const userId = getUserId(context)
       const contacts = await context.prisma.user({ id: userId }).contacts()
-      const contact = contacts.find(contact => contact.id === args.id)
-      return await context.prisma.deleteContact({id: contact.id})
+      const contact = contacts.find((contact) => contact.id === args.id)
+      return await context.prisma.deleteContact({ id: contact.id })
     },
     updateContact: async (parent, args, context, info) => {
       const userId = getUserId(context)
-      const {id, companyId, ...data} = args
+      const { id, companyId, ...data } = args
       return await context.prisma.updateContact({
         data: {
-          company: { connect: { id: companyId }},
+          company: { connect: { id: companyId } },
           ...data,
         },
-        where: { id: id},
+        where: { id: id },
       })
-    }
-  }
+    },
+  },
 }
 
 module.exports = {
